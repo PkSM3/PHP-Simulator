6ea02a245c711f5744d2fd948390ef69f948cad6
PkSM3 <pokesam3@gmail.com>
2013-12-18 18:44:35 -0300

    gamma ok

--- php/distribuciones/continuas/Gamma.php
+++ php/distribuciones/continuas/Gamma.php
@@ -10,28 +10,60 @@
  *
  * @author jaime
  */
+//
+//$seed=rand();
+//include_once("../../UniformC.php");
+//$unif01 = new UniformC();
+//$s = $unif01->go($seed);
+
+
+$a = pow(7, 5);
+$m = pow(2, 31) - 1;
+$b = 0;
+
+$seed=rand();
+include('../VariablesAleatoriasUniforme.php');
+$va = new VariablesAleatoriasUniforme($a, $b, $m,$seed);
+$s=$va->nextUniforme();
+$la=10;
+$g=new Gamma($la,$s);
+echo $g->gen1();
 class Gamma {
     //put your code here
     private $lambda;
-    private $b;
     private $gamma = array();
     private $va;
 
-    public function __construct($lambda, $b, VariablesAleatoriasUniforme $va) {
+    public function __construct($lambda, $va) {
         $this->lambda = $lambda;
-        $this->b = $b;
         $this->va = $va;
     }
 
+    public function gen1(){
+        // Algorithm:
+        // http://en.wikipedia.org/wiki/Lanczos_approximation
+        $x=$this->va;
+        $ret=(1.000000000190015 +
+                76.18009172947146 / ($x + 1) + 
+                -86.50532032941677 / ($x + 2) + 
+                -1.231739572450155 / ($x + 4) +
+                1.208650973866179e-3 / ($x + 5) + 
+                -5.395239384953e-6 / ($x + 6));
+        
+        return $ret * sqrt(2*pi())/$x * pow($x + 5.5, $x+.5) * exp(-$x-5.5);
+    }
+    
     public function generar($n){
-        $vaUniforme = $this->va->generar($this->b);
-        $mult = 1.0;
-        for($i = 0; $i < $this->b; $i++)
-            $mult *= $vaUniforme[$i];
         for($i = 0; $i < $n; $i++){
-            $this->gamma[$i] = -1*  $this->lambda*log($mult);
+            $seed=$this->va->nextUniforme();
+            $this->exponencial[$i] = -1*  $this->lambda*(log($seed));
         }
-        return $this->gamma;
+//        for($i = 0; $i < $this->b; $i++)
+//            $mult *= $vaUniforme[$i];
+//        for($i = 0; $i < $n; $i++){
+//            $this->gamma[$i] = -1*  $this->lambda*log($mult);
+//        }
+        //return $ret;//$this->gamma;
     }
 
     public function setB($b){
--- php/distribuciones/test.php
+++ php/distribuciones/test.php
@@ -14,7 +14,7 @@ include_once('./continuas/Exponencial.php');
 $a = pow(7, 5);
 $m = pow(2, 31) - 1;
 $b = 0;
-
+echo "a: ".$a.", m: ".$m.", b: ".$b;
 $va = new VariablesAleatoriasUniforme($a, $b, $m);
 
 $uniforme = new Exponencial(10, $va);
